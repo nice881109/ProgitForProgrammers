a72fd3a3dd884c5c417fd5dca9c4908acad8f206
nice881109 <10742011@ntub.edu.tw>
2023-11-14 16:32:13 +0800

    add to show the staging area

--- ProGitProgrammers/ProGitProgrammers/Program.cs
+++ ProGitProgrammers/ProGitProgrammers/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in visual Studio");
+Console.WriteLine("hi!");
