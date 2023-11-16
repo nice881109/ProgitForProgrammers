2a58e879bf8bcf78c7bbcde4e88c120f18d9643c
nice881109 <10742011@ntub.edu.tw>
2023-11-16 14:25:33 +0800

    test merge

--- ProGitProgrammers/ProGitProgrammers/book.cs
+++ ProGitProgrammers/ProGitProgrammers/book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,7 @@ namespace ProGitProgrammers
         public string Title { get; set; }
         public List<string> Authors { get; set; }
         public DateTime PublicationDate { get; set; }
+
+        public string test_branvh = "1";
     }
 }
